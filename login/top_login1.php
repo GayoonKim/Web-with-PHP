e16200cc64ae66919aff54ccf87c6d3fe84be12e
GayoonKim <gayunkim.1@gmail.com>
2022-10-04 19:04:56 +0900

    로그인, 회원가입 버튼 파일 명 수정

--- login/top_login1.php
+++ login/top_login1.php
@@ -2,7 +2,7 @@
     <?php
     if (!isset($_SESSION['userid'])) {
     ?>
-        <a href="view/login_form.php">로그인</a> | <a href="view/insertForm.php">회원가입</a>
+        <a href="./login/login_form.php">로그인</a> | <a href="./login/insertForm.php">회원가입</a>
     <?php
     }
     else {
